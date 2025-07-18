6cd526e7db89470fc21609edc1ce641b6a4d08ac
Jan + Scott <janryan.hidalgo+nicholasscott.campos@gcash.com>
2025-07-18 14:24:58 +0800

    Added lab6

---
+++ session2/lab6/src/main/java/com/example/demo/myfirstapi/model/Product.java
@@ -0,0 +1,67 @@
+package com.example.demo.myfirstapi.model;
+import java.util.*;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(Long id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(price, product.price) == 0 &&
+                Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
